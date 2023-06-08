e2e4cd6f8ef8bf48903bfbdf835fa447227adffe
yu-xiangang <1922669677@qq.com>
2023-06-08 08:27:55 +0800

    data race problem for 2C

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -302,9 +302,7 @@ func (rf *Raft) Snapshot(index int, snapshot []byte) {
 }
 
 func (rf *Raft) StartAppendEntries(heart bool) {
-	//if rf.state != Leader {
-	//	return
-	//}
+
 	// 并行向其他节点发送心跳或者日志，让他们知道此刻已经有一个leader产生
 	//DPrintf(111, "%v: detect the len of peers: %d", rf.SayMeL(), len(rf.peers))
 	for i, _ := range rf.peers {
